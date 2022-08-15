7e6d6200a2fe495e28cd807c8400e731b74e2d19
Isabella <isabella.y.hu@gmail.com>
2022-08-15 20:29:11 +1000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc (array)
+    array.sort
+end
 
+def sort_array_desc (array)
+    array.sort {|a,b| b <=> a}
+end
+
+def sort_array_char_count (array)
+    array.sort_by {|c| c.length}
+end
+
+def swap_elements (array)
+    array.shuffle(random: Random.new(1))
+end
+
+def reverse_array (array)
+    array.reverse
+end
+
+def kesha_maker (array)
+    array.map do |word| 
+        word[2]="$"
+        word
+    end
+end
+
+def find_a (array)
+    array.select {|word| word.start_with?("a")}
+end
+
+def sum_array (array)
+    array.sum
+end
+
+def add_s (array)
+    array.map.with_index do |word,index|
+        if index == 1
+            word
+        else
+            word << "s"
+        end
+    end
+end
